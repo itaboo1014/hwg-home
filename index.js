e69cce32b4076abe1c1cbf567cce9b8ed506ce01
itaboo1014 <69159009+itaboo1014@users.noreply.github.com>
2021-01-09 22:59:29 +0900

    Add files via upload

--- index.js
+++ index.js
@@ -6,4 +6,24 @@ $(function() {
     });
     //ヘッダーアイコンをクリックすると、ページ最上部まで自動スクロールされる処理。
 
+
+    //ヘッダーメニューボタンを押した時にボタンが変化しリンクメニューが表示/非表示され、背景の固定が、有効/無効になる処理。
+    var scrollPosition;
+    $("#header-menu-btn").on("click", function() {
+        if ($(this).hasClass('header-menu-btn-active')) {
+            $(this).html('<span class="fa fa-bars"></span>');
+            $(this).removeClass('header-menu-btn-active');
+            $("#main-menu").addClass("main-menu-close");
+            $('body').removeClass('fixed').css({ 'top': 0 });
+            window.scrollTo(0, scrollPosition);
+        } else {
+            $(this).html('<span class="fa fa-times"></span>');
+            $(this).addClass('header-menu-btn-active');
+            $("#main-menu").removeClass("main-menu-close");
+            scrollPosition = $(window).scrollTop();
+            $('body').addClass('fixed').css({ 'top': -scrollPosition });
+        }
+    });
+    //ヘッダーメニューボタンを押した時にボタンが変化しリンクメニューが表示/非表示され、背景の固定が、有効/無効になる処理。
+
 });
